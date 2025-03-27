930a3cf3b1b4d2245c9b5754879f8dab9cdfb28a
Cecilia Andrade <ceciliandradesz@outlook.com>
2025-03-26 22:41:51 -0300

    Tela de cadastro

---
+++ views/register_page/register.py
@@ -0,0 +1,17 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.screenmanager import Screen
+from kivy.core.window import Window
+
+Builder.load_file("register.kv")
+
+class RegisterScreen(Screen):
+    pass
+
+class Registre(MDApp):
+    def build(self):
+        Window.size = (360, 640)  
+        return RegisterScreen()     
+
+if __name__ == '__main__':
+    Registre().run()
