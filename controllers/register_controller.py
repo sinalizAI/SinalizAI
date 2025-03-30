931d8a6c96d7217bd35ebaa41b1c8eac790ddbe0
Hebert Rocha <hebertsrocha79@gmail.com>
2025-03-29 23:09:44 -0300

    reorganizando novas telas

--- controllers/register_controller.py
+++ controllers/register_controller.py
@@ -3,7 +3,7 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.core.window import Window
 
-Builder.load_file("register.kv")
+Builder.load_file("views/register_page/register.kv")
 
 class RegisterScreen(Screen):
     pass
